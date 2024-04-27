240ec9ecb7e55edb5bfe206ef1616968c9a20d6e
Lihuén <86395729+lihuencarranza@users.noreply.github.com>
2024-04-27 17:44:22 +0000

    error al terminar signals

--- shell/sh.c
+++ shell/sh.c
@@ -3,6 +3,8 @@
 #include "readline.h"
 #include "runcmd.h"
 
+void sigchld_handler();
+
 char prompt[PRMTLEN] = { 0 };
 
 // runs a shell command
@@ -16,11 +18,37 @@ run_shell()
 			return;
 }
 
+void
+sigchld_handler()
+{
+	pid_t pid;
+	int status;
+	
+	while ((pid = waitpid(0, &status, WNOHANG)) > 0) {
+		print_status_info(parsed_pipe);
+	}
+}
+
+// sets the signal handlers
+static void 
+set_signal_handlers(){
+	// register signal
+	struct sigaction sa = { .sa_handler = sigchld_handler,
+		                .sa_flags = SA_RESTART };
+
+	if (sigaction(SIGCHLD, &sa, NULL) == -1) {
+		perror("sigaction");
+		exit(1);
+	}
+}
+
 // initializes the shell
 // with the "HOME" directory
 static void
 init_shell()
 {
+	set_signal_handlers();
+
 	char buf[BUFLEN] = { 0 };
 	char *home = getenv("HOME");
 
