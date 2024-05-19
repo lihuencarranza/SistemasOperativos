1108046cf59657c5305540b1a46b1f62ea87d496
lihuencarranza <lcarranza@fi.uba.ar>
2024-05-19 12:47:21 -0300

    modularizacion sched priorities y RR

--- sched/kern/sched.c
+++ sched/kern/sched.c
@@ -7,40 +7,84 @@
 
 void sched_halt(void);
 
+unsigned times_scheduler_called = 0;
+
 #ifdef SCHED_PRIORITIES
-	void reduce_priority(struct Env *env){
-		if(env->env_priority>MIN_PRIORITY_LEVEL){
-			env->env_priority--;
-		}
-	}
+void reduce_priority(struct Env *env) {
+    if (env->env_priority > MIN_PRIORITY_LEVEL) {
+        env->env_priority--;
+    }
+}
 
-	void print_first_3(){
-		for (int i = 0; i < NENV; i++) {
-			if (envs[i].env_status == ENV_RUNNABLE) {
-				// cprintf("envs[%d].env_priority = %d\n", envs[i].env_id,envs[i].env_priority);
-			}
-		}
-	}
 
-	void boost_priority(){
-		// cprintf("Boosting priority\n");
-		for (int i = 0; i < NENV; i++) {
-			if (envs[i].env_status == ENV_RUNNABLE) {
-				envs[i].env_priority = MAX_PRIORITY_LEVEL;
-			}
-		}
-	}
+void boost_priority() {
+    for (int i = 0; i < NENV; i++) {
+        if (envs[i].env_status == ENV_RUNNABLE) {
+            envs[i].env_priority = MAX_PRIORITY_LEVEL;
+        }
+    }
+}
 
+struct Env *sched_priorities(void) {
+    struct Env *highest_priority = NULL;
+
+    if (times_scheduler_called % 30 == 0)
+        boost_priority();
+
+    // Find the highest priority environment
+    for (int i = 0; i < NENV; i++) {
+        if (envs[i].env_status == ENV_RUNNABLE) {
+            if (highest_priority == NULL || envs[i].env_priority > highest_priority->env_priority) {
+                highest_priority = &envs[i];
+            }
+        }
+    }
+
+    if (highest_priority && highest_priority->env_runs != 0) {
+        reduce_priority(highest_priority);
+    }
+
+    return highest_priority;
+}
 #endif
 
+#ifdef SCHED_ROUND_ROBIN
+struct Env *sched_round_robin(void) {
+    struct Env *idle = NULL;
 
-unsigned times_scheduler_called = 0;
+    if (!curenv) {
+        int i = 0;
+        while (i < NENV && envs[i].env_status != ENV_RUNNABLE) {
+            i++;
+        }
+        if (envs[i].env_status == ENV_RUNNABLE)
+            idle = &envs[i];
+    } else {
+        int i = ENVX(curenv->env_id) + 1;
+        while (curenv != &envs[i]) {
+            if (i >= NENV) {
+                i = 0;
+            } else {
+                if (envs[i].env_status == ENV_RUNNABLE) {
+                    idle = &envs[i];
+                    break;
+                }
+                i++;
+            }
+        }
+    }
+
+    return idle;
+}
+#endif
+
 
 void
 sched_yield(void)
 {
 	struct Env *idle = NULL;
 	times_scheduler_called++;
+	
 #ifdef SCHED_ROUND_ROBIN
 	// Implement simple round-robin scheduling.
 	//
@@ -55,78 +99,32 @@ sched_yield(void)
 	// another CPU (env_status == ENV_RUNNING). If there are
 	// no runnable environments, simply drop through to the code
 	// below to halt the cpu.
-	// cprintf("Running round robin scheduler\n");
 	// Your code here - Round robin
-	if (!curenv) {
-		int i = 0;
-		while (i < NENV && envs[i].env_status != ENV_RUNNABLE) {
-			i++;
-		}
-		if (envs[i].env_status == ENV_RUNNABLE)
-			idle = &envs[i];
-	} else {
-		int i = ENVX(curenv->env_id) + 1;
-		while (curenv != &envs[i]) {
-			if (i >= NENV) {
-				i = 0;
-			} else {
-				if (envs[i].env_status == ENV_RUNNABLE) {
-					idle = &envs[i];
-					break;
-				}
-				i++;
-			}
-		}
-	}
-	if (idle) {
-		env_run(idle);
-	} else if (curenv && curenv->env_status == ENV_RUNNING &&
-	           curenv->env_cpunum == cpunum()) {
-		env_run(curenv);
-	}
+
+	idle = sched_round_robin();
 #endif
 
 
 #ifdef SCHED_PRIORITIES
+
 	// Implement simple priorities scheduling.
 	//
 	// Environments now have a "priority" so it must be consider
 	// when the selection is performed.
-	// cprintf("Times scheduler called: %d\n",times_scheduler_called);
-	if (times_scheduler_called%30==0)
-		boost_priority();
-	//  print_first_3();
-	struct Env *highest_priority = NULL;
-	for (int i = 0; i < NENV; i++) {
-		if (envs[i].env_status == ENV_RUNNABLE) {
-			if (highest_priority == NULL || envs[i].env_priority > highest_priority->env_priority) 
-				highest_priority = &envs[i];
-		}
-	}
-
-	if(highest_priority && highest_priority->env_runs!=0){
-		reduce_priority(highest_priority);
-	}
-
-	// Run the highest priority environment
-	if (highest_priority) {
-		env_run(highest_priority);
-	}
-	// If there is no runnable environment, run the current environment
-	if (curenv && curenv->env_status == ENV_RUNNING && curenv->env_cpunum == cpunum()) {
-		env_run(curenv);
-	}
 
 	// Be careful to not fall in "starvation" such that only one
 	// environment is selected and run every time.
 
 	// Your code here - Priorities
+	idle = sched_priorities();
 #endif
 
 	// Without scheduler, keep runing the last environment while it exists
-	// if (curenv) {
-	// 	env_run(curenv);
-	// }
+	if (idle) {
+        env_run(idle);
+    } else if (curenv && curenv->env_status == ENV_RUNNING && curenv->env_cpunum == cpunum()) {
+        env_run(curenv);
+    }
 
 	// sched_halt never returns
 	sched_halt();
