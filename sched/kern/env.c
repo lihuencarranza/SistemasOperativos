07de0542978bf2cbcee53933b054ea9463a9052b
lihuencarranza <lcarranza@fi.uba.ar>
2024-05-18 23:26:35 -0300

    agrego ifdef de constante

--- sched/kern/env.c
+++ sched/kern/env.c
@@ -227,7 +227,9 @@ env_alloc(struct Env **newenv_store, envid_t parent_id)
 	e->env_type = ENV_TYPE_USER;
 	e->env_status = ENV_RUNNABLE;
 	e->env_runs = 0;
+	#ifdef SCHED_PRIORITIES
 	e->env_priority = MAX_PRIORITY_LEVEL;
+	#endif
 
 	// Clear out all the saved register state,
 	// to prevent the register values
