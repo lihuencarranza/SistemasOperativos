5a7a5033ae7fa6dcf47e42d771b0ea0bd1e43b66
lihuencarranza <lcarranza@fi.uba.ar>
2024-06-11 19:17:20 -0300

    arreglando formateo

--- fisopfs/fisopfs.h
+++ fisopfs/fisopfs.h
@@ -16,23 +16,23 @@
 
 // Esta es la estructura principal para guardar un archivo en memoria.
 // Agregaremos lo necesario para que el filesystem funcione como esperamos.
-// 
-typedef struct inode{
-	size_t file_size; // Tamaño del archivo
-	int uid; // User ID
-	int type; // Tipo de archivo (IS_FILE o IS_DIR)
-	int nlink; // Cantidad de links
-	mode_t mode; // Modo
+//
+typedef struct inode {
+	size_t file_size;  // Tamaño del archivo
+	int uid;           // User ID
+	int type;          // Tipo de archivo (IS_FILE o IS_DIR)
+	int nlink;         // Cantidad de links
+	mode_t mode;       // Modo
 
-	char file_name[MAX_PATH]; // Nombre del archivo/directorio. Si es root, es "/".
-	char file_parent[MAX_PATH]; // Path del directorio que lo contiene o path del directorio padre,  o si es root está vacío.
-	char file_path[MAX_PATH]; // Path completo del archivo/directorio.
-	char file_content[MAX_CONTENT]; // Contenido del archivo
+	char file_name[MAX_PATH];  // Nombre del archivo/directorio. Si es root, es "/".
+	char file_parent[MAX_PATH];  // Path del directorio que lo contiene o path
+	                             // del directorio padre,  o si es root está vacío.
+	char file_path[MAX_PATH];  // Path completo del archivo/directorio.
+	char file_content[MAX_CONTENT];  // Contenido del archivo
 
-	
-}inode_t;
+} inode_t;
 
 // Esta estructura guarda los inodos de los archivos que tenemos en memoria.
-typedef struct super_block{
-	struct inode inodes[MAX_INODES]; // Inodos
-}super_block_t;
+typedef struct super_block {
+	struct inode inodes[MAX_INODES];  // Inodos
+} super_block_t;
--- fisopfs/test.c
+++ fisopfs/test.c
@@ -7,37 +7,44 @@
 #include <unistd.h>
 
 
-void create_simple(char* path, int* result){
-    result =  creat(path, S_IFREG | S_IRWXU | S_IRWXG | S_IRWXO);
-    assert(result > 2);
+void
+create_simple(char *path, int *result)
+{
+	result = creat(path, S_IFREG | S_IRWXU | S_IRWXG | S_IRWXO);
+	assert(result > 2);
 }
 
-void create_already_exists(char* path, int* result){
-    result =  creat(path, S_IFREG | S_IRWXU | S_IRWXG | S_IRWXO);
-    assert(result == 0);
-}
-
-
-void creation_tests() {
-    // si el archivo ya existe devuelve 0
-    // si el archivo devuleve -1 es por un error
-    // si devuelve 1 o 2 puede que se haya cerrado el stdouto stderr
-    printf("======== PRUEBAS DE CREACIÓN DE ARCHIVIOS ===========");
-
-    char *path = "test.txt";
-
-    int result; 
-    
-    create_simple(path, &result);
-    create_already_exists(path, &result);
-
-    close(path);
+void
+create_already_exists(char *path, int *result)
+{
+	result = creat(path, S_IFREG | S_IRWXU | S_IRWXG | S_IRWXO);
+	assert(result == 0);
 }
 
 
-int main() {
-    
-    creation_test();
+void
+creation_tests()
+{
+	// si el archivo ya existe devuelve 0
+	// si el archivo devuleve -1 es por un error
+	// si devuelve 1 o 2 puede que se haya cerrado el stdouto stderr
+	printf("======== PRUEBAS DE CREACIÓN DE ARCHIVIOS ===========");
 
-    return 0;
+	char *path = "test.txt";
+
+	int result;
+
+	create_simple(path, &result);
+	create_already_exists(path, &result);
+
+	close(path);
+}
+
+
+int
+main()
+{
+	creation_test();
+
+	return 0;
 }
